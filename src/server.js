e74b70a830a607db82097842a2eb9dd15f846918
Smith Júnior <smith.junior@icloud.com>
2019-10-02 23:23:12 -0300

    Started project aircnc backend cofigured the express

---
+++ src/server.js
@@ -0,0 +1,17 @@
+require('dotenv').config()
+
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const server = require('http').Server(app)
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (request, response) => {
+  return response.json({
+    message: 'Server running...'
+  })
+})
+
+server.listen(process.env.PORT || 3333)
