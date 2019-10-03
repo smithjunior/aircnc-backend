bc36ff0f02fa9a8d0d26653bc2149b041d85da4d
Smith Júnior <smith.junior@icloud.com>
2019-10-02 23:45:39 -0300

    Added mongo connection

---
+++ src/routes.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const routes = express.Router()
+
+routes.post('/users', (request, response) => {
+  return response.json(request.body)
+})
+
+module.exports = routes
--- src/server.js
+++ src/server.js
@@ -1,12 +1,21 @@
 require('dotenv').config()
 
 const express = require('express')
+const mongoose = require('mongoose')
+
 const cors = require('cors')
+const routes = require('./routes')
 const app = express()
 const server = require('http').Server(app)
 
+mongoose.connect(process.env.MONGO_DB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+
 app.use(cors())
 app.use(express.json())
+app.use(routes)
 
 app.get('/', (request, response) => {
   return response.json({
